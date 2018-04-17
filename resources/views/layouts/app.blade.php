1774c3292251625336d5c4532f0438993ab43101
Choo <choo.chenwei95@gmail.com>
2018-04-17 11:10:12 +0800

    Tempo removed jquery, affects logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,12 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Scripts -->
+
+    <script src="{{ asset('js/app.js') }}"></script>
+    {{-- <script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> --}}
+    @yield('scripts')
 </head>
 <body>
     <div id="app">
@@ -76,10 +82,5 @@
 
     </div>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <script src="{{ asset('js/app.js') }}"></script>
-    @yield('scripts')
 </body>
 </html>
